18cb1625367477c0f99c7c09b23e19a549deaaf9
Miguel <miguelj92@outlook.com>
2022-10-11 22:12:12 -0500

    request lib setup

--- pricetracker.py
+++ pricetracker.py
@@ -1,4 +1,11 @@
-from cgi import test
+#import time
+#import requests #for sending request to amazon
+#from bs4 import BeautifulSoup #for getting values from  response...
+from urllib.request import Request, urlopen
 
-
-testfsf
+req = Request(
+    url='https://www.amazon.com.mx/Consola-Digital-Horizon-Forbidden-WestTM/dp/B0B5JCBX6M', 
+    headers={'User-Agent': 'Mozilla/5.0'}
+)
+webpage = urlopen(req).read()
+print(webpage)
